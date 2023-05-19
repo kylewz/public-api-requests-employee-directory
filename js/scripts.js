02cf6f625b8057075f650de4b182101cdb924782
Kyle <83605851+kylewz@users.noreply.github.com>
2023-05-18 19:07:16 -0600

    Update scripts.js
    Update modal html to use employee array from api request

--- js/scripts.js
+++ js/scripts.js
@@ -12,25 +12,27 @@ fetch(
     let allCards = gallery.children;
     const cardsArray = Array.from(allCards);
     console.log(gallery);
+
     gallery.addEventListener('click', (e) => {
       console.log(e.relatedTarget);
-      // console.log(cardsArray.indexOf(e.currentTarget));
+      generateModal(randomUserArray);
+      console.log(cardsArray.indexOf(e.currentTarget));
     });
   });
 
 function generateUserCards(array) {
   let galleryHTML = array
     .map(
-      (results, index) =>
+      (employee, index) =>
         `
         <div class="card" id="card-${index}">
             <div class="card-img-container">
-                <img class="card-img" src="${results.picture.medium}" alt="profile picture">
+                <img class="card-img" src="${employee.picture.medium}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 class="card-name cap">${results.name.first} ${results.name.last}</h3>
-                <p class="card-text">${results.email}</p>
-                <p class="card-text cap">${results.location.city}, ${results.location.state}</p>
+                <h3 class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="card-text">${employee.email}</p>
+                <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
             </div>
         </div>
         `
@@ -42,23 +44,23 @@ function generateUserCards(array) {
     .insertAdjacentHTML('beforeend', galleryHTML);
 }
 
-function generateModal() {
+function generateModal(array) {
   let modalHTML = `
-        <div class="modal-container">
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">name</h3>
-                    <p class="modal-text">email</p>
-                    <p class="modal-text cap">city</p>
-                    <hr>
-                    <p class="modal-text">(555) 555-5555</p>
-                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                    <p class="modal-text">Birthday: 10/21/2015</p>
-                </div>
-            </div>
-        </div>`;
+  <div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${array[0].picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${array[0].name.first} ${array[0].name.last}</h3>
+            <p class="modal-text">${array[0].email}</p>
+            <p class="modal-text cap">${array[0].location.city}</p>
+            <hr>
+            <p class="modal-text">${array[0].phone}</p>
+            <p class="modal-text">${array[0].location.street.number} ${array[0].location.street.name}, ${array[0].location.city}, ${array[0].location.state} ${array[0].location.postcode}</p>
+            <p class="modal-text">Birthday: ${array[0].dob.date}</p>
+        </div>
+    </div>
+  </div>`;
 
-  document.getElementById('gallery').insertAdjacentHTML('afterend', modalHTML);
+  document.getElementById('gallery').insertAdjacentHTML('beforeend', modalHTML);
 }
