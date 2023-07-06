e7eef11043f81de4a50c300b8f6478565968c355
kylewz <kyle.z@hey.com>
2023-07-06 13:01:20 -0600

    Update scripts.js
    Add comments, begin refactor of event listener on modal buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+/**
+ * Treehouse FSJS Techdegree, Project 5
+ *
+ * Using the Random User Generator API (randomuser.me), generates 12 employees
+ * for an online "Awesome Startup Directory".
+ */
+
+// Variables for the fetch function
 let gallery = document.getElementById('gallery');
 
 fetch(
@@ -6,14 +14,14 @@ fetch(
   .then((response) => response.json())
   .then((data) => {
     const randomUserArray = data.results;
-    // console.log(randomUserArray);
     randomUserArray.forEach((element) => {
       element.dob.date = formatDOB(element.dob.date);
     });
 
+    // Create and display user cards
     generateUserCards(randomUserArray);
 
-    // create array for use in generating modals per card clicked
+    // Create array for use in generating modals
     let userCards = gallery.children;
     const cardsArray = Array.from(userCards);
 
@@ -25,7 +33,7 @@ fetch(
     );
   });
 
-// Formats the API user DOB for display in modal as mm/dd/yyyy
+// Formats the retrieved API user DOB for display in modal as mm/dd/yyyy
 function formatDOB(dob) {
   let year = dob.slice(0, 4);
   let month = dob.slice(5, 7);
@@ -34,6 +42,7 @@ function formatDOB(dob) {
   return `${month}/${day}/${year}`;
 }
 
+// Function to generate HTML for user cards based on Random User API data
 function generateUserCards(array) {
   let galleryHTML = array
     .map(
@@ -56,6 +65,8 @@ function generateUserCards(array) {
   gallery.insertAdjacentHTML('beforeend', galleryHTML);
 }
 
+// Generate modal HTML based on clicked user card, include buttons "Prev" and
+// "Next" to generate adjacent card modals, close modal with "X" button
 function generateModal(array, index) {
   let modalHTML = `
   <div class="modal-container">  
@@ -80,27 +91,48 @@ function generateModal(array, index) {
 
   gallery.insertAdjacentHTML('afterend', modalHTML);
 
+  // Variables used for event listeners
+  let prevBtn = document.getElementById('modal-prev');
+  let nextBtn = document.getElementById('modal-next');
+  let modalContainer = document.querySelector('.modal-container');
+
   //Check boundaries, prev and next buttons unavailable if at edges
   if (index === 0) {
-    document.getElementById('modal-prev').style.display = 'none';
+    prevBtn.style.display = 'none';
   }
   if (index === array.length - 1) {
-    document.getElementById('modal-next').style.display = 'none';
+    nextBtn.style.display = 'none';
   }
 
+  /*
   //Add listener to Prev and Next buttons to display respective user modal
-  document.getElementById('modal-prev').addEventListener('click', () => {
-    document.querySelector('.modal-container').remove();
+  prevBtn.addEventListener('click', () => {
+    modalContainer.remove();
     generateModal(array, index - 1);
   });
 
-  document.getElementById('modal-next').addEventListener('click', () => {
-    document.querySelector('.modal-container').remove();
+  nextBtn.addEventListener('click', () => {
+    modalContainer.remove();
     generateModal(array, index + 1);
   });
 
   //Close the modal with close btn
   document.getElementById('modal-close-btn').addEventListener('click', () => {
-    document.querySelector('.modal-container').remove();
+    modalContainer.remove();
+  });
+  */
+
+  modalContainer.addEventListener('click', (e) => {
+    console.log(e.target);
+    console.log(e.currentTarget);
+
+    modalContainer.remove();
+
+    if (e.target.id === 'modal-next') {
+      generateModal(array, index + 1);
+    } else if (e.target.id === 'modal-prev') {
+      generateModal(array, index - 1);
+    } else if (e.target.id === 'modal-close-btn') {
+    }
   });
 }
